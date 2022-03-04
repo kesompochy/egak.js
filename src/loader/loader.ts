c136d8242db19f1783059b7461f2601630c063b0
kesompochy <kesompochy@gmail.com>
2022-03-05 03:03:17 +0900

    tasksとresourcesをprivate化

--- src/loader/loader.ts
+++ src/loader/loader.ts
@@ -1,33 +1,39 @@
 export default class Loader {
-    static resources: Map<string, any> = new Map();
-    static tasks: Array<Promise<unknown>> = [];
+    private static _resources: Map<string, any> = new Map();
+    private static _tasks: Array<Promise<unknown>> = [];
     private static _loadThen: Function = function(){};
     constructor(){
     }
     static add(id: string, src: string): Loader{
         const promise = this._promiseLoadingImage(id, src);
-        this.tasks.push(promise);
+        this._tasks.push(promise);
         
         return this;
     }
     static loadAll(): void{
-        Promise.all(this.tasks)
+        Promise.all(this._tasks)
             .then(()=>{this._loadThen();});
     }
     private static _promiseLoadingImage(id: string, src: string): Promise<any>{
         const promise = new Promise((resolve)=>{
             fetch(src).then(res=>res.blob())
                     .then(blob=>createImageBitmap(blob))
-                    .then(data=>this.resources.set(id, data))
+                    .then(data=>this._resources.set(id, data))
                     .then(img=>resolve(img));
         });
         return promise;
     }
-    static loadThen(func: Function){
+    static loadThen(func: Function): Loader{
         this._loadThen = func;
+
+        if(this._tasks.length === 0){
+            func();
+        }
+
+        return this;
     }
 
     static get(id: string): WebGLTexture{
-        return this.resources.get(id);
+        return this._resources.get(id);
     }
 }
