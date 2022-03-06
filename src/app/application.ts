909ccdb382db49c56f1afa4fd6ed18d0d336caab
kesompochy <kesompochy@gmail.com>
2022-03-06 12:35:40 +0900

    flushを消してみるテスト

--- src/app/application.ts
+++ src/app/application.ts
@@ -88,7 +88,7 @@ export default class App {
     render(): void{
         this.renderer.render(this.baseStage);
 
-        this.renderer.flush();
+        //this.renderer.flush();
     }
     clearScreen(r: number = 0, g: number = 0, b: number = 0, a?: number): void{
         this.renderer.clear(r, g, b, a);
