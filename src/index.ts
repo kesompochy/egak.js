e143b41e29e411e75d6a160ebb135ca70d7b638f
kesompochy <kesompochy@gmail.com>
2022-03-06 11:51:24 +0900

    Loaderをexportsに追加

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import Sprite from './display/sprite';
 import Texture from './texture/texture';
 import Stage from './display/stage';
 import Text from './display/text';
+import Loader from './loader/loader';
 
-export { App, Sprite, Texture, Stage, Text }; 
+export { App, Sprite, Texture, Stage, Text, Loader }; 
