6e8b1a7ef0d6561315d15dc802f4d3ee738b7ab1
kesompochy <kesompochy@gmail.com>
2022-03-09 08:49:17 +0900

    graphicsをpolygonとcircleに分類

---
+++ src/graphics/circle/circle.ts
@@ -0,0 +1,16 @@
+import Graphics from "../graphics";
+export default class Circle extends Graphics{
+    readonly graphicsType: string = 'circle';
+    readonly shaderType: string = 'circle';
+    radius: number;
+    center: {x: number, y: number};
+    constructor(x: number, y: number, radius: number, r: number, g: number, b: number, a: number){
+        super();
+        this.vertices.push([x-radius, y-radius, r, g, b, a]);
+        this.vertices.push([x-radius, y+radius, r, g, b, a]);
+        this.vertices.push([x+radius, y-radius, r, g, b, a]);
+        this.vertices.push([x+radius, y+radius, r, g, b, a]);
+        this.radius = radius;
+        this.center = {x: x, y: y};
+    }
+}
