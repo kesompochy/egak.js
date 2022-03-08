640caed672d46d71b6e0b72cc46076a852acc22f
kesompochy <kesompochy@gmail.com>
2022-03-08 16:19:56 +0900

    rectangleを実装

--- src/graphics/rectangle.ts
+++ src/graphics/rectangle.ts
@@ -1,12 +1,12 @@
 import Graphics from './graphics';
 
 export default class Rectangle extends Graphics{
-    type: string = 'rectangle';
-    constructor(x: number, y: number, w: number, h: number, r: number, g: number, b: number, a: number){
+    type = 'rectangle';
+    constructor(x: number, y: number, w: number, h: number, ...color: number[][]){
         super();
-        this.vertices.push([x, y, r, g, b, a]);
-        this.vertices.push([x+w, y, r, g, b, a]);
-        this.vertices.push([x+w, y+h, r, g, b, a]);
-        this.vertices.push([x, y+h, r, g, b, a]);
+        this.vertices.push([x, y, color[0][0], color[0][1], color[0][2], color[0][3]]);
+        this.vertices.push([x, y+h, color[1][0], color[1][1], color[1][2], color[1][3]]);
+        this.vertices.push([x+w, y, color[2][0], color[2][1], color[2][2], color[2][3]]);
+        this.vertices.push([x+w, y+h, color[3][0], color[3][1], color[3][2], color[3][3]]);
     }
 }
