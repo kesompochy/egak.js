ae75defecec11356b13e50281dc4ad832d7e5384
kesompochy <kesompochy@gmail.com>
2022-03-08 15:54:08 +0900

    triangleの実装

--- src/graphics/graphics.ts
+++ src/graphics/graphics.ts
@@ -1,6 +1,8 @@
 import Stage from '../display/stage';
 
+
 export default abstract class Graphics extends Stage {
-    abstract readonly type: string
+    abstract readonly type: string;
+    vertices: Array<Array<number>> = [];
 }
 
