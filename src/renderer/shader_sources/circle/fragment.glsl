eee371c616b141e59c9bf36469b201a073f95d98
kesompochy <kesompochy@gmail.com>
2022-03-09 10:54:21 +0900

    円をなめらかに表示できるようにした

--- src/renderer/shader_sources/circle/fragment.glsl
+++ src/renderer/shader_sources/circle/fragment.glsl
@@ -20,11 +20,14 @@ void main() {
    
    float dx = v_position.x - u_center.x;
    float dy = v_position.y - u_center.y;
-
    float angle = u_clockwize*fract(atan(dy, dx)/PI2 + 1.);
 
-   if((dx*dx + dy*dy < u_radius*u_radius && u_clockwize*angle > u_startAngle && u_clockwize*angle <= u_endAngle)){
-      outColor = v_color * vec4(1, 1, 1, u_opacity);
+   float dist = distance(u_center, v_position)/u_radius;
+   float delta = fwidth(dist);
+   float alpha = smoothstep(1.-delta, 1., dist);
+
+   if((u_clockwize*angle > u_startAngle && u_clockwize*angle <= u_endAngle)){
+      outColor = mix(v_color*vec4(1, 1, 1, u_opacity), outColor, alpha);
    } else {
       discard;
    }
