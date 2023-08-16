68a2e045e5653627bb71ff94f96eb997f72eb418
kesompochy <kesompochy@gmail.com>
2023-08-16 13:48:14 +0900

    use ts for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import vitePluginRaw from 'vite-plugin-raw';
 
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'EGAK',
+      formats: ['es', 'umd'],
       fileName: (format) => `egak${format === 'umd' ? '.min' : ''}.js`,
     },
   },
