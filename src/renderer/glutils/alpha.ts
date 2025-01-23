95d701348129aefe1fc6ba8d22075ec2c94c2eda
kesompochy <kesompochy@gmail.com>
2025-01-24 00:06:40 +0900

    stack alpha by default

--- src/renderer/glutils/alpha.ts
+++ src/renderer/glutils/alpha.ts
@@ -1,5 +1,12 @@
-export function enableAlpha(gl: WebGL2RenderingContext) {
+export function enableAlpha(gl: WebGL2RenderingContext, separateAlpha: boolean = true) {
   gl.enable(gl.BLEND);
-  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+  if (!separateAlpha) {
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+  } else {
+    gl.blendFuncSeparate(
+      gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA,  // RGB
+      gl.ONE, gl.ONE_MINUS_SRC_ALPHA         // Alpha
+    );
+  }
   gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
 }
